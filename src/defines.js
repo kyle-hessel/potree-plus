5fc5d634a8b121b43f64ac78805741428a3bf9d7
Kyle Hessel <KHessel@PENNONI.com>
2021-07-07 16:02:12 -0400

    Changed the unitspermeter for feet from 3.28084 to 3.280833333.

--- src/defines.js
+++ src/defines.js
@@ -51,6 +51,6 @@ export const TreeType = {
 
 export const LengthUnits = {
 	METER: {code: 'm', unitspermeter: 1.0},
-	FEET: {code: 'ft', unitspermeter: 3.28084},
+	FEET: {code: 'ft', unitspermeter: 3.280833333},
 	INCH: {code: '\u2033', unitspermeter: 39.3701}
 };
